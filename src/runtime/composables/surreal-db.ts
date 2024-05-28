58f695054a4518f04099a5613d3da0af5afb8743
Sandros94 <sandro.circi@digitoolmedia.com>
2024-05-29 01:06:23 +0200

    feat(items): use useAsyncData to catch input errors

--- src/runtime/composables/surreal-db.ts
+++ src/runtime/composables/surreal-db.ts
@@ -1,9 +1,10 @@
 import { joinURL } from 'ufo'
+import { hash } from 'ohash'
 
 import type { Overrides } from '../types'
-import type { UseFetchOptions } from '#app'
+import type { AsyncDataOptions } from '#app'
 import type { MaybeRefOrGetter } from '#imports'
-import { createError, toValue, useNuxtApp, useSurrealFetch } from '#imports'
+import { computed, createError, toValue, useAsyncData, useNuxtApp, useSurrealFetch } from '#imports'
 
 export function useSurrealDB(overrides?: Overrides) {
   const { $surrealFetch, $surrealFetchOptionsOverride } = useNuxtApp()
@@ -13,49 +14,58 @@ export function useSurrealDB(overrides?: Overrides) {
       table: MaybeRefOrGetter<string>
       id?: MaybeRefOrGetter<string>
     },
-    options: Omit<UseFetchOptions<T>, 'method'> & Overrides & {
+    options: AsyncDataOptions<T> & Overrides & {
+      key?: string
       method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+      body?: RequestInit['body'] | Record<string, any>
     } = {
       ...overrides,
       method: 'GET',
+      key: undefined,
     },
   ) {
     const _record = toValue(record)
     const {
+      key,
       method,
       body,
       database,
       token,
       ...opts
     } = options
-
     const { id, table } = typeof _record === 'string'
       ? { table: _record.split(':')[0], id: _record.split(':')[1] }
       : _record
-
     const _table = toValue(table)
-    if (!_table) {
-      throw createError({
-        statusCode: 400,
-        statusMessage: 'Table name is required',
-      })
-    }
-    if ((method === 'POST' || method === 'PUT' || method === 'PATCH') && !body) {
-      throw createError({
-        statusCode: 400,
-        statusMessage: 'Body is required for POST, PUT, and PATCH methods',
-      })
-    }
-
-    return useSurrealFetch<T>(() => joinURL('key', _table, toValue(id || '')), {
-      ...opts,
-      ...$surrealFetchOptionsOverride({
-        database,
-        token,
-      }),
-      method,
-      body,
+    const _key = computed(() => {
+      return key ?? 'D_' + hash(['surrealItems', toValue(_table), toValue(id || '')])
     })
+
+    return useAsyncData<T>(_key.value, () => {
+      const endpoint = computed(() => joinURL('key', _table, toValue(id || '')))
+
+      if (!_table) {
+        throw createError({
+          statusCode: 400,
+          statusMessage: 'Table name is required',
+        })
+      }
+      if ((method === 'POST' || method === 'PUT' || method === 'PATCH') && !body) {
+        throw createError({
+          statusCode: 400,
+          statusMessage: 'Body is required for POST, PUT, and PATCH methods',
+        })
+      }
+
+      return $surrealFetch<T>(endpoint.value, {
+        ...$surrealFetchOptionsOverride({
+          database,
+          token,
+        }),
+        method,
+        body,
+      })
+    }, opts)
   }
 
   async function sql<T = any>(sql: string, ovr?: Overrides) {
