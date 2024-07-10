5914442bcbc8743a9d7f3b0daced6bfd2728d14e
Sandros94 <sandro.circi@digitoolmedia.com>
2024-07-10 17:51:36 +0200

    fix: query server util types

--- src/runtime/server/utils/surreal-db.ts
+++ src/runtime/server/utils/surreal-db.ts
@@ -1,11 +1,11 @@
 import type { H3Event } from 'h3'
 
-import type { QueryRpcResponse, RpcParams } from '../../types'
+import type { QueryResponse, RpcParams } from '../../types'
 import { type ServerOverrides, useSurrealRPC } from './surreal-fetch'
 
 export function useSurrealDB(event: H3Event, overrides?: ServerOverrides) {
   // query [ sql, vars ]
-  async function query<T = any, R = QueryRpcResponse<T>>(
+  async function query<T = any, R = QueryResponse<T>>(
     sql: RpcParams<any, 'query'>[0],
     vars?: RpcParams<any, 'query'>[1],
     options?: ServerOverrides,
