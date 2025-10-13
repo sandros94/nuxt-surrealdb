7b1d9512d95aa03cb25ccb857385ce380fff50f0
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-10-13 17:33:07 +0200

    up

--- playground/app/plugins/prepare-local.client.ts
+++ playground/app/plugins/prepare-local.client.ts
@@ -3,14 +3,14 @@ export default defineNuxtPlugin(() => {
     await client.use({
       namespace: 'test',
       database: 'test',
-    }),
+    })
     await client.query('DEFINE TABLE IF NOT EXISTS test; UPSERT test SET name = "from-wasm-mem" WHERE name = "from-wasm-mem";')
   })
   surrealHooks.hook('surrealdb:local:connected', async ({ client }) => {
     await client.use({
       namespace: 'test',
       database: 'test',
-    }),
+    })
     await client.query('DEFINE TABLE IF NOT EXISTS test; UPSERT test SET name = "from-wasm-local" WHERE name = "from-wasm-local";')
   })
 })
--- playground/server/plugins/prepare-local.ts
+++ playground/server/plugins/prepare-local.ts
@@ -3,14 +3,14 @@ export default defineNitroPlugin(() => {
     await client.use({
       namespace: 'test',
       database: 'test',
-    }),
+    })
     await client.query('DEFINE TABLE IF NOT EXISTS test; UPSERT test SET name = "from-node-mem" WHERE name = "from-node-mem";')
   })
   surrealHooks.hook('surrealdb:local:connected', async ({ client }) => {
     await client.use({
       namespace: 'test',
       database: 'test',
-    }),
+    })
     await client.query('DEFINE TABLE IF NOT EXISTS test; UPSERT test SET name = "from-node-local" WHERE name = "from-node-local";')
   })
 })
