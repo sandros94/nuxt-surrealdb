09accc87a9f88f307bc120be738f97c3b57a1aea
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-11-04 01:04:41 +0100

    chore: update playground

--- playground/server/api/surreal/node/mem.get.ts
+++ playground/server/api/surreal/node/mem.get.ts
@@ -1,7 +1,5 @@
-export default defineEventHandler(async (event) => {
-  const client = await useSurrealMemory(event)
-
-  // TOTO: SurrealDB in-memory seems to have issues with storing data, but it does not crash
+export default defineEventHandler(async () => {
+  const client = await useSurrealMemory()
 
   const [res] = await client.query('SELECT * FROM test;').json().collect(0)
   return res
